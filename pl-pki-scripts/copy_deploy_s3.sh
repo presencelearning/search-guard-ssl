1086220136da9c4811dd2584205fb5c58e0a93c0
Aaron Cowdin <aaron.cowdin@gmail.com>
2016-03-23 14:27:34 -0700

    SEems that kibana likes pems.

--- pl-pki-scripts/copy_deploy_s3.sh
+++ pl-pki-scripts/copy_deploy_s3.sh
@@ -2,4 +2,6 @@
 
 #CA crts
 aws --region us-west-2 s3 cp ca/root-ca.crt s3://presencelearning-devops/letsencrypt/svc/ca/root-ca.crt --sse
+aws --region us-west-2 s3 cp ca/root-ca.pem s3://presencelearning-devops/letsencrypt/svc/ca/root-ca.pem --sse
 aws --region us-west-2 s3 cp ca/signing-ca.crt s3://presencelearning-devops/letsencrypt/svc/ca/signing-ca.crt --sse
+aws --region us-west-2 s3 cp ca/signing-ca.pem s3://presencelearning-devops/letsencrypt/svc/ca/signing-ca.pem --sse
